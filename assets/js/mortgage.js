cba9dcf910ab88c1e250b3a55f4be76141fb6965
cfredric <chris.p.fredrickson@gmail.com>
2020-12-20 12:13:32 -0500

    Assorted refactorings

--- assets/js/mortgage.js
+++ assets/js/mortgage.js
@@ -119,14 +119,14 @@
         let sum = 0;
         let equity = down_payment();
         const schedule = [];
-        for (let i = 0; i < n; ++i) {
+        for (const month of d3.range(n)) {
             const principal = price() - equity;
             const interest_payment = interest_rate() / 12 * principal;
             const pmi_payment = equity < 0.2 * price() ? pmi() : 0;
             sum += monthly_payment + pmi_payment;
             equity += (monthly_payment - interest_payment);
             schedule.push({
-                month: i + 1,
+                month: month + 1,
                 interest: interest_payment,
                 principal: monthly_payment - interest_payment,
                 principal_balance: price() - equity,
@@ -148,7 +148,7 @@
     };
 
     const bisect_month = (data, x, mouse_x) => {
-        const bisect = d3.bisector(d => d.month).left;
+        const bisect = d3.bisector((d) => d.month).left;
         const month = x.invert(mouse_x);
         const index = bisect(data, month, 1);
         const a = data[index - 1];
@@ -157,32 +157,12 @@
     };
 
     const buildPaymentScheduleChart = (schedule) => {
-        const identify_payment_type = (y, mouse_y, datum) => {
-            const y_target = y.invert(mouse_y);
-            let cumulative = 0;
-            for (const [idx, key] of keys.entries()) {
-                if (cumulative + datum[key] >= y_target) {
-                    return idx;
-                }
-                cumulative += datum[key];
-            }
-            return keys.length - 1;
-        };
-
         // set the dimensions and margins of the graph
         const margin = {top: 50, right: 100, bottom: 120, left: 100};
         const width = 900 - margin.left - margin.right;
         const height = 450 - margin.top - margin.bottom;
 
-        d3.select("#schedule_viz")
-            .select('svg')
-            .remove();
-        const svg = d3.select("#schedule_viz")
-            .append('svg')
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", `translate(${margin.left}, ${margin.top})`);
+        const svg = makeSvg("schedule_viz", width, height, margin);
 
         const stack = d3.stack()
             .keys(keys)
@@ -190,36 +170,7 @@
             .offset(d3.stackOffsetNone)
             (schedule);
 
-        // Add X axis
-        const x = d3.scaleLinear()
-            .domain(d3.extent(schedule, (d) => d.month))
-            .range([0, width]);
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x)
-                .tickValues(d3.range(0, schedule.length, 12))
-            );
-
-        // text label for the x axis
-        svg.append("text")
-            .attr("transform", `translate(${width / 2}, ${height + margin.top})`)
-            .style("text-anchor", "middle")
-            .text("Month");
-
-        const y = d3.scaleLinear()
-            .domain([0, d3.max(schedule, (d) => d3.sum(keys.map((k) => d[k])))*1.25])
-            .range([height, 0]);
-        svg.append("g")
-            .call(d3.axisLeft(y));
-
-        // text label for the y axis
-        svg.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 0 - margin.left)
-            .attr("x", 0 - (height / 2))
-            .attr("dy", "1em")
-            .style("text-anchor", "middle")
-            .text("Monthly Payment");
+        const {x, y} = makeAxes(svg, schedule, width, height, margin, "Monthly Payment", d3.sum);
 
         // Add the area
         svg.append("g")
@@ -233,134 +184,29 @@
                 .y1((d) => y(d[1]))
             );
 
-        const tooltip = svg.append("g");
-
-        svg.on("touchmove mousemove", function(event) {
-            const pointer = d3.pointer(event, this);
-            const datum = bisect_month(schedule, x, pointer[0]);
-            const payment_type_idx = identify_payment_type(y, pointer[1], datum);
-
-            const value = keys.map((k) => `${display_keys[k]}: ${fmt.format(datum[k])}` + '\n').join('') +
-                `Month: ${formatMonthNum(datum.month)}`;
-            tooltip
-                .attr("transform", `translate(${x(datum.month)},${pointer[1]})`)
-                .call(callout, value, payment_type_idx);
+        makeTooltip(svg, schedule, x, (mouse_y, datum) => {
+            const y_target = y.invert(mouse_y);
+            let cumulative = 0;
+            for (const [idx, key] of keys.entries()) {
+                if (cumulative + datum[key] >= y_target) {
+                    return idx;
+                }
+                cumulative += datum[key];
+            }
+            return keys.length - 1;
         });
 
-        svg.on("touchend mouseleave", () => tooltip.call(callout, null, null));
-
-        const legend = svg.append('g')
-            .attr('class', 'legend')
-            .attr('transform', `translate(${width - 200}, -50)`);
-        legend.selectAll('rect')
-            .data(keys)
-            .enter()
-            .append('rect')
-            .attr('x', 0)
-            .attr('y', (_, i) => i * 18)
-            .attr('width', 12)
-            .attr('height', 12)
-            .attr('fill', (d, i) => colors[d]);
-
-        legend.selectAll('text')
-            .data(keys)
-            .enter()
-            .append('text')
-            .text((d) => display_keys[d])
-            .attr('x', 18)
-            .attr('y', (d, i) => i * 18)
-            .attr('text-anchor', 'start')
-            .attr('alignment-baseline', 'hanging');
-    };
-
-    const callout = (g, value, payment_type_idx) => {
-        if (!value) return g.style("display", "none");
-
-        g
-            .style("display", null)
-            .style("pointer-events", "none")
-            .style("font", "12px sans-serif");
-
-        const path = g.selectAll("path")
-            .data([null])
-            .join("path")
-            .attr("fill", "white")
-            .attr("stroke", "black");
-
-        const text = g.selectAll("text")
-            .data([null])
-            .join("text")
-            .call(text => text
-                .selectAll("tspan")
-                .data((value + "").split(/\n/))
-                .join("tspan")
-                .attr("x", 0)
-                .attr("y", (d, i) => `${i * 1.1}em`)
-                .style("font-weight", (_, i) => i === payment_type_idx ? "bold" : null)
-                .text(d => d));
-
-        const {x, y, width: w, height: h} = text.node().getBBox();
-
-        text.attr("transform", `translate(${-w / 2},${15 - y})`);
-        path.attr("d", `M${-w / 2 - 10},5H-5l5,-5l5,5H${w / 2 + 10}v${h + 20}h-${w + 20}z`);
+        makeLegend(svg, width, (d) => colors[d]);
     };
 
     const buildCumulativeChart = (data) => {
-        const identify_payment_type = (y, mouse_y, datum) => {
-            const y_target = y.invert(mouse_y);
-            const sorted = keys.map((key) => ({key, value: datum[key]}))
-                .sort((a, b) => a.value - b.value);
-            for (const [idx, elt] of sorted.entries()) {
-                if (y_target <= elt.value && (idx === sorted.length - 1 || sorted[idx + 1].value >= y_target)) {
-                    return keys.indexOf(elt.key);
-                }
-            }
-            return keys.indexOf(sorted[sorted.length - 1].key);
-        };
-
         const margin = {top: 50, right: 100, bottom: 120, left: 100};
         const width = 900 - margin.left - margin.right;
         const height = 450 - margin.top - margin.bottom;
 
-        d3.select("#cumulative_viz")
-            .select('svg')
-            .remove();
-        const svg = d3.select("#cumulative_viz")
-            .append("svg")
-            .attr('width', width + margin.left + margin.right)
-            .attr('height', height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", `translate(${margin.left}, ${margin.top})`);
+        const svg = makeSvg("cumulative_viz", width, height, margin);
 
-        const x = d3.scaleLinear()
-            .domain(d3.extent(data, (d) => d.month))
-            .range([0, width]);
-        svg.append("g")
-            .attr("transform", `translate(0, ${height})`)
-            .call(d3.axisBottom(x)
-                .tickValues(d3.range(0, data.length, 12))
-            );
-
-        // text label for the x axis
-        svg.append("text")
-            .attr("transform", `translate(${width / 2}, ${height + margin.top})`)
-            .style("text-anchor", "middle")
-            .text("Month");
-
-        const y = d3.scaleLinear()
-            .domain([0, d3.max(data, (d) => d3.max(keys.map((k) => d[k])))*1.25])
-            .range([height, 0]);
-        svg.append("g")
-            .call(d3.axisLeft(y));
-
-        // text label for the y axis
-        svg.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 0 - margin.left)
-            .attr("x", 0 - (height / 2))
-            .attr("dy", "1em")
-            .style("text-anchor", "middle")
-            .text("Cumulative Payment");
+        const {x, y} = makeAxes(svg, data, width, height, margin, "Cumulative Payment", d3.max);
 
         const area = d3.area()
             .curve(d3.curveMonotoneX)
@@ -382,12 +228,81 @@
             .attr("d", (d) => area(d.values))
             .style("fill", (d) => transparent(colors[d.key]));
 
-        const tooltip = svg.append('g');
+        makeTooltip(svg, data, x, (mouse_y, datum) => {
+            const y_target = y.invert(mouse_y);
+            const sorted = keys.map((key) => ({key, value: datum[key]}))
+                .sort((a, b) => a.value - b.value);
+            const elt = sorted.find(
+                (elt, idx, arr) => y_target <= elt.value && (idx === arr.length - 1 || arr[idx + 1].value >= y_target)) ??
+                sorted[sorted.length - 1];
+            return keys.indexOf(elt.key)
+        });
+
+        makeLegend(svg, width, (d) => transparent(colors[d]));
+    };
+
+    const transparent = (color) => `${color}aa`;
+
+    const formatMonthNum = (m) => {
+        const years = Math.floor(m / 12);
+        const months = m % 12;
+        return (years ? `${years}y ` : '') + `${months}mo`;
+    };
+
+    const makeSvg = (div_id, width, height, margin) => {
+        d3.select(`#${div_id}`)
+            .select('svg')
+            .remove();
+        return d3.select(`#${div_id}`)
+            .append('svg')
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    };
+
+    const makeAxes = (svg, data, width, height, margin, y_label, y_domain_fn) => {
+        // Add X axis
+        const x = d3.scaleLinear()
+            .domain(d3.extent(data, (d) => d.month))
+            .range([0, width]);
+        svg.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(x)
+                .tickValues(d3.range(0, data.length, 12))
+            );
+
+        // text label for the x axis
+        svg.append("text")
+            .attr("transform", `translate(${width / 2}, ${height + margin.top})`)
+            .style("text-anchor", "middle")
+            .text("Month");
+
+        const y = d3.scaleLinear()
+            .domain([0, d3.max(data, (d) => y_domain_fn(keys.map((k) => d[k])) * 1.25)])
+            .range([height, 0]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        // text label for the y axis
+        svg.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text(y_label);
+
+        return {x, y};
+    };
+
+    const makeTooltip = (svg, data, x, identify_payment_type) => {
+        const tooltip = svg.append("g");
 
         svg.on("touchmove mousemove", function(event) {
             const pointer = d3.pointer(event, this);
             const datum = bisect_month(data, x, pointer[0]);
-            const payment_type_idx = identify_payment_type(y, pointer[1], datum);
+            const payment_type_idx = identify_payment_type(pointer[1], datum);
 
             const value = keys.map((k) => `${display_keys[k]}: ${fmt.format(datum[k])}` + '\n').join('') +
                 `Month: ${formatMonthNum(datum.month)}`;
@@ -398,6 +313,41 @@
 
         svg.on("touchend mouseleave", () => tooltip.call(callout, null, null));
 
+        const callout = (g, value, payment_type_idx) => {
+            if (!value) return g.style("display", "none");
+
+            g
+                .style("display", null)
+                .style("pointer-events", "none")
+                .style("font", "12px sans-serif");
+
+            const path = g.selectAll("path")
+                .data([null])
+                .join("path")
+                .attr("fill", "white")
+                .attr("stroke", "black");
+
+            const text = g.selectAll("text")
+                .data([null])
+                .join("text")
+                .call((text) => text
+                    .selectAll("tspan")
+                    .data((value + "").split(/\n/))
+                    .join("tspan")
+                    .attr("x", 0)
+                    .attr("y", (_, i) => `${i * 1.1}em`)
+                    .style("font-weight", (_, i) => i === payment_type_idx ? "bold" : null)
+                    .text((d) => d)
+                );
+
+            const {x, y, width: w, height: h} = text.node().getBBox();
+
+            text.attr("transform", `translate(${-w / 2},${15 - y})`);
+            path.attr("d", `M${-w / 2 - 10},5H-5l5,-5l5,5H${w / 2 + 10}v${h + 20}h-${w + 20}z`);
+        };
+    };
+
+    const makeLegend = (svg, width, color) => {
         const legend = svg.append('g')
             .attr('class', 'legend')
             .attr('transform', `translate(${width - 200}, -50)`);
@@ -409,7 +359,7 @@
             .attr('y', (_, i) => i * 18)
             .attr('width', 12)
             .attr('height', 12)
-            .attr('fill', (d, i) => transparent(colors[d]));
+            .attr('fill', color);
 
         legend.selectAll('text')
             .data(keys)
@@ -417,22 +367,15 @@
             .append('text')
             .text((d) => display_keys[d])
             .attr('x', 18)
-            .attr('y', (d, i) => i * 18)
+            .attr('y', (_, i) => i * 18)
             .attr('text-anchor', 'start')
             .attr('alignment-baseline', 'hanging');
     };
 
-    const transparent = (color) => `${color}aa`;
-
-    const formatMonthNum = (m) => {
-        const years = Math.floor(m / 12);
-        const months = m % 12;
-        return (years ? `${years}y ` : '') + `${months}mo`;
-    };
-
     const initFieldsFromUrl = () => {
         const url = new URL(location.href);
-        if ([
+        let has_value = false;
+        for (const [name, elt] of [
             ["price", price_input],
             ["hoa", hoa_input],
             ["down_payment", down_payment_input],
@@ -441,11 +384,12 @@
             ["property_tax", property_tax_input],
             ["hoi", homeowners_insurance_input],
             ["closing_cost", closing_cost_input],
-        ].map(([name, elt]) => {
+        ]) {
             const value = url.searchParams.get(name);
             elt.value = value ?? '';
-            return value !== null;
-        }).some((x) => x)) {
+            has_value ||= value !== null;
+        }
+        if (has_value) {
             setContents();
         }
     };
